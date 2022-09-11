278bc71b74f3b0781fbfb362ed14fbb96a1ae9a5
GodfredApps <appiahgodfred8@gmail.com>
2022-09-11 10:15:59 -0700

    Task 6 updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+/**
+ * main- Entry point
+ * Return: always returns 0
+ */
 
 int main(void)
 {
 	int i;
 	double d;
 	char c;
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));        
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of an duoble is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
-	return(0);
+	return (0);
 }
+
