d63826d12ab412412429a1499a4bd64dda1fcb41
GodfredApps <appiahgodfred8@gmail.com>
2022-09-12 01:11:22 +0000

    Task 6 updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+/**
+ * main- entry point
+ * Return: always returns 0
+ *
+ */
 
 int main(void)
 {
 	int i;
 	double d;
 	char c;
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));        
-	printf("The size of an duoble is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
-	return(0);
+	long int b;
+	long long int e;
+	float f;
+
+	printf("The size of an int is: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("The size of an duoble is: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("The size of an char is: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of an long is: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("The size of an 2xlong is: %lu byte(s)\n", (unsigned long) sizeof(e));
+	printf("The size of an float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
+
