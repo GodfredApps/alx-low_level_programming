2b592a6fd84069ef3460cb500c28fe6fcab012c4
GodfredApps <appiahgodfred8@gmail.com>
2022-10-01 03:18:53 +0000

    Task 1 updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point to print number of arguemnets passed
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: Always success
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
 
 int main(int argc, char const *argv[])
 {
 	(void)argv;
-	
 	printf("%i\n", argc - 1);
 	return (0);
 }
