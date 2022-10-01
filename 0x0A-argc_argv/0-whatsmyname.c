ceeecb32d292d068ef032ff113ddc4423e683bde
GodfredApps <appiahgodfred8@gmail.com>
2022-10-01 01:41:20 +0000

    Task 0 completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point for function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: Always success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s/n", argv[0]);
+
+	return (0);
+}
+
