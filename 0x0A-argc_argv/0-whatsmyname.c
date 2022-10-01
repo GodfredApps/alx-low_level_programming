a69a26ab5ccfd4bb5c804d9bc9f09151aa01ef57
GodfredApps <appiahgodfred8@gmail.com>
2022-10-01 01:44:14 +0000

    Task 0 updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
  * Return: Always success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 
-	printf("%s/n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
