b0bf99ccb1d66121fe5148e3cf225d6c564a3571
GodfredApps <appiahgodfred8@gmail.com>
2022-10-11 05:52:49 +0000

    Header file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,6 +26,11 @@ struct dog
 
 typedef struct dog dog_t;
 
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
